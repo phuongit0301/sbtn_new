b4428505228e3868959da9d55e0ffe3abd433fff
Phan Thanh Phuong <phuongit0301@gmail.com>
2016-12-23 19:54:30 +0700

    Update run android

--- android/app/src/main/java/com/sbtn_new/MainApplication.java
+++ android/app/src/main/java/com/sbtn_new/MainApplication.java
@@ -28,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
             new ReactVideoPackage(),
             new VectorIconsPackage()
       );
